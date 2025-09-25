d55428198726ad6b0e44b133d1d2eff9dc43cfeb
Linus Schürtz <post@linus-schuertz.de>
2025-09-25 10:12:10 +0200

    chore: remove colemak and lower layout
    Signed-off-by: Linus Schürtz <post@linus-schuertz.de>

--- keyboards/gmmk/pro/rev1/iso/keymaps/jonavin/rules.mk
+++ keyboards/gmmk/pro/rev1/iso/keymaps/jonavin/rules.mk
@@ -8,5 +8,4 @@ IDLE_TIMEOUT_ENABLE = yes
 STARTUP_NUMLOCK_ON = yes
 ENCODER_DEFAULTACTIONS_ENABLE = no
 
-COLEMAK_LAYER_ENABLE = yes  #Enable Colemak layer / set to no to disable
 INVERT_NUMLOCK_INDICATOR = yes
